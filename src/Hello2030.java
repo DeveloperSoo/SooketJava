b94218ebe3a2f53d8d6d1ed56c7b8e44fd1a692b
user1 <user1@DESKTOP-27PHPOM>
2024-12-05 17:35:59 +0900

    [ADD] Day01 첫째날 이클립스 코드
    1. Hello2030, 첫 자바 실행

---
+++ src/Hello2030.java
@@ -0,0 +1,13 @@
+
+public class Hello2030 {
+	public static void main(String[] args){
+		int n = 2030;
+		double second = 11.18;
+		String name = "트럼프"; //문자열은 String
+		char mbti = 'T'; // 문자는 char 와 '' 사용
+		boolean result = true;
+		
+		// 숫자(정수/실수), 문자(문자/문자열), 참/거짓  
+		System.out.println("헬로 "+n);	
+	}
+}
